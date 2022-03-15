381506996df633f8d290814eefac24a407ba2752
dolbung <jj600159@gmail.com>
2022-03-15 14:30:02 +0900

    12번

--- 조인범/12.c
+++ 조인범/12.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	int a,b;
-	
-	scanf("%d %d" , &a  , &b );
-	printf("%d %d" , a  , b);
-	
-	return 0;
+    
+    float tech;
+    scanf("%f" , &tech);
+    printf("%f" , tech);
+    return 0;
+    
 }
